537e4d0dbcfaa868f399c5f53e7ab9113faa52bf
maxtalarico <marceltalarico@gmail.com>
2016-09-19 20:35:17 -0300

    Starting development.

---
+++ blink_mobile/app/src/main/java/com/example/marcel/blink_mobile/Home.java
@@ -0,0 +1,49 @@
+package com.example.marcel.blink_mobile;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.AdapterView;
+
+public class Home extends Fragment implements OnClickListener{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        /*
+        Button transButton = (Button)rootView.findViewById(R.id.button);
+        transButton.setOnClickListener(this);
+
+        Button recButton = (Button)rootView.findViewById(R.id.button2);
+        recButton.setOnClickListener(this);
+        */
+
+        return rootView;
+    }
+
+    public void onClick(View v) {
+        Fragment fragment = null;
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        //do what you want to do when button is clicked
+       switch (v.getId()) {
+           case R.id.btnContinuar:
+               break;
+
+           default:
+               break;
+        }
+
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
+    }
+}
